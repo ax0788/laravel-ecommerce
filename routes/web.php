665585fed7bd69a589fe978edd698eb5361b73be
Ahmed Yusuf <axmed.y788@gmail.com>
2022-06-12 11:25:49 +0800

    First commit

---
+++ routes/web.php
@@ -0,0 +1,41 @@
+<?php
+
+use GuzzleHttp\Middleware;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminController;
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+// Admin Routes
+Route::group(['prefix' => 'admin', 'middleware' => ['admin:admin']], function () {
+    Route::get('/login', [AdminController::class, 'loginForm']);
+    Route::post('/login', [AdminController::class, 'store'])->name('admin.login');
+});
+// Admin guarded login
+Route::middleware(['auth:sanctum,admin', 'verified'])->group(function () {
+    Route::get('/admin/dashboard', function () {
+        return view('admin.index');
+    })->name('dashboard');
+});
+
+// User Routes
+
+// User Guard Auth login
+Route::middleware(['auth:sanctum,web', 'verified'])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
